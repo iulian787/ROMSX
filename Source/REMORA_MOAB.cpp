1b5bc7e4695dd428ea008fc2048b0aec16a57025
Iulian Grindeanu <iulian@mcs.anl.gov>
2024-09-28 20:04:12 -0500

    try to create vertices in parallel in MOAB
    	it is wrong, because the vertices are duplicated
    		we need to iterate differenly in the model

--- Source/REMORA_MOAB.cpp
+++ Source/REMORA_MOAB.cpp
@@ -8,6 +8,7 @@ void
 REMORA::InitMOABMesh()
 {
     moab::Interface * mbi = new( std::nothrow ) moab::Core;
+    moab::ParallelComm* pco = new moab::ParallelComm( mbi,  ParallelContext::CommunicatorSub() );
     // Number of cells in this "domain" at this level
     std::vector<int> n_cells;
 
@@ -36,7 +37,7 @@ REMORA::InitMOABMesh()
     int num_vertices   = (nx+1)*(ny+1)*(nz+1);
     int num_cells = nx * ny * nz;
 
-    std::vector<double> coords(3*num_cells);// just the center of cells?
+    std::vector<double> coords;// just the center of cells?
     long unsigned goffset = 0;
     long unsigned glen    = 0;
     int icell = 0;
@@ -48,19 +49,25 @@ REMORA::InitMOABMesh()
             for (auto k1 = 0; k1 < grids[lev][i].length(0); ++k1) {
                 for (auto k2 = 0; k2 < grids[lev][i].length(1); ++k2) {
                     for (auto k3 = 0; k3 < grids[lev][i].length(2); ++k3) {
-                        coords[3*icell] = gridloc.lo(0)+geom[lev].CellSize(0)*static_cast<Real>(k1);
-                        coords[3*icell+1] = gridloc.lo(1)+geom[lev].CellSize(1)*static_cast<Real>(k2);
-                        coords[3*icell+2] = gridloc.lo(2)+geom[lev].CellSize(2)*static_cast<Real>(k3);
+                        coords.push_back(gridloc.lo(0)+geom[lev].CellSize(0)*static_cast<Real>(k1));
+                        coords.push_back(gridloc.lo(1)+geom[lev].CellSize(1)*static_cast<Real>(k2));
+                        coords.push_back(gridloc.lo(2)+geom[lev].CellSize(2)*static_cast<Real>(k3));
                         icell++;
                     }
                 }
             }
+            goffset += glen;
+            glen = grids[lev][i].length(0)*grids[lev][i].length(1)*grids[lev][i].length(2);
         }
     }
     moab::Range cell_centers;
-    moab::ErrorCode rval = mbi->create_vertices( &coords[0], num_cells, cell_centers );
+    moab::ErrorCode rval = mbi->create_vertices( &coords[0], icell, cell_centers );
+    moab::EntityHandle moabSet;
+    mbi->create_meshset( moab::MESHSET_SET, moabSet );
+    mbi->add_entities(moabSet, cell_centers);
 
-    rval = mbi->write_file("cell_centers.h5m");
+    std::string pw( "PARALLEL=WRITE_PART" );
+    rval = mbi->write_file("cell_centers.h5m", 0, pw.c_str(), &moabSet, 1);
 
 
     return;
